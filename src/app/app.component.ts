2b10a3165617f3fdd0f22d8a64f6173776281bd3
Guillermo Murúa <icguillermo.murua@americanwell.com>
2024-02-19 10:45:14 -0300

    Added translations (portugues and spanish)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { TranslateModule } from '@ngx-translate/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { FooterComponent } from '../footer/footer.component';
 
 @Component({
@@ -16,4 +16,9 @@ import { FooterComponent } from '../footer/footer.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(private translate: TranslateService) {
+    this.translate.setDefaultLang('en');
+    this.translate.use(this.translate.getBrowserLang()!);
+  }
+}
