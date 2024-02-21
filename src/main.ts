8ff456eb9338da3cbf49835c8f749c3642af739e
Guillermo Murúa <icguillermo.murua@americanwell.com>
2024-02-21 08:55:04 -0300

    Added firebase configs and initialization

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,26 @@ import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import * as Cesium from 'cesium';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: process.env['FIREBASE_API_KEY'],
+  authDomain: process.env['FIREBASE_AUTH_DOMAIN'],
+  projectId: process.env['FIREBASE_PROJECT_ID'],
+  storageBucket: process.env['FIREBASE_STORAGE_BUCKET'],
+  messagingSenderId: process.env['FIREBASE_MESSAGING_SENDER_ID'],
+  appId: process.env['FIREBASE_APP_ID'],
+  measurementId: process.env['FIREBASE_MEASUREMENT_ID']
+};
+
+
+
 
 //@ts-ignore
 window['CESIUM_BASE_URL'] = '/assets/cesium/';
@@ -10,4 +30,10 @@ window['CESIUM_BASE_URL'] = '/assets/cesium/';
 window['Cesium'] = Cesium
 
 bootstrapApplication(AppComponent, appConfig)
+  .then((ref) => {
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+    const analytics = getAnalytics(app);
+    console.log('Firebase initialized', app, analytics);
+  })
   .catch((err) => console.error(err));
